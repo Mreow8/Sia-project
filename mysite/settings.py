87397b395ebf0a5cd3af455dc022f139d2da2349
Mreow8 <honzelchavez100@gmail.com>
2025-11-27 08:38:14 +0800

    Add login feature

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ FIREBASE_CLIENT_CONFIG = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sia-project-8pgs.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
